8ba3cffb3e2380b6d3460cb903479ca8edb60b40
전현배 <sa04108@naver.com>
2026-09-04 22:09:01 +0900

    fix(db): revert preparation preview columns

---
+++ migrations/0033_yielding_saracen.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `backtest_preparation_jobs` ADD `preview_schedule_hash` text;--> statement-breakpoint
+ALTER TABLE `backtest_preparation_jobs` ADD `preview_union_symbols_json` text;
---
+++ migrations/0034_revert_preparation_preview_columns.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `backtest_preparation_jobs` DROP COLUMN `preview_schedule_hash`;--> statement-breakpoint
+ALTER TABLE `backtest_preparation_jobs` DROP COLUMN `preview_union_symbols_json`;
